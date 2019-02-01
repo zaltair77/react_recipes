06175143d79d3c2e47f6e329768f14b69974788b
zaltair77 <holb3838@fredonia.edu>
2019-02-01 13:44:01 -0500

    Mount root component

--- index.js
+++
@@ -1,5 +0,0 @@
-import log from './log';
-
-
-
-log('hello world');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  context: path.join(__dirname, 'src'),
   entry: './index.js',
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, 'dist'),
     filename: './bundle.js'
   },
   module: {
@@ -13,5 +16,11 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'body'
+    })
+  ]
+};
